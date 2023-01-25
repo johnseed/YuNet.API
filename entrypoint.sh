c4545f82c4fafa67c72899d672e579bbb00c2da6
John <nowyoutry@outlook.com>
2023-01-26 02:44:14 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@
 . /opt/conda/etc/profile.d/conda.sh
 
 # activate the environment
-# conda activate face
-source face/bin/activate
+conda activate face
+# source face/bin/activate
 # exec the cmd/command in this process, making it pid 1
 exec "$@"
 # exec "cd /app && uvicorn --host 0.0.0.0 --port 8000 fastapi-server:app"
