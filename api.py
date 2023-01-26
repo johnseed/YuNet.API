a31f154038434d97634987e07d4b972baf727026
John <nowyoutry@outlook.com>
2023-01-27 01:05:15 +0800

    Add NudeNet and InsightFace api.

--- api.py
+++ api.py
@@ -13,6 +13,8 @@ from fastapi.staticfiles import StaticFiles
 from multiprocessing import Pool
 
 import yunet_sdk
+import nudenet_sdk
+import insightface_sdk
 
 # import seqlog
 # seqlog.set_global_log_properties(
@@ -58,21 +60,123 @@ async def redoc_html():
 def read_root():
     return {"swagger": "/docs"}
 
-@app.post("/detect_face", summary='detect face', tags=['face'], response_description=""
+@app.post("/detect_face", summary='detect face', tags=['YuNet'], response_description="""
+ the location of a face and 5 facial landmarks. The format of each row is as follows:
+x1, y1, w, h, x_re, y_re, x_le, y_le, x_nt, y_nt, x_rcm, y_rcm, x_lcm, y_lcm
+, where x1, y1, w, h are the top-left coordinates, width and height of the face bounding box, {x, y}_{re, le, nt, rcm, lcm} stands for the coordinates of right eye, left eye, nose tip, the right corner and left corner of the mouth respectively.
+"""
                                                                       "返回结果：\n\n"
                                                                       "Faces: bouding box [0:4]\n\n"
                                                                       "landmarks: 5 landmarks [4:14]\n\n"
                                                                       "confidence [-1]\n\n"
                                                                       "")
-def sst_qa(detectParam: yunet_sdk.DetectionParam = Body(..., description="文件路径参数", example = {
-	"input": "/app/data/photomode_05052022_000846.png",
-	"output": "/app/data/photomode_05052022_000846-faces.png"
+def detect_face(detectParam: yunet_sdk.DetectionParam = Body(..., description="文件路径参数", example = {
+	"input": "/C/Intel/1.png",
+	"output": "/C/Intel/1-faces.png",
+    "save": True
 }
 )):
     # logging.info("sst_qa, {param}", param=qaParam.__dict__)
     if(not validate_multi_file_path([detectParam.input, detectParam.output])):
         return False
     return yunet_sdk.detect_face(detectParam)
+
+@app.post("/recognize_face", summary='recognize face', tags=['SFace'], response_description=""
+                                                                      "返回结果：\n\n"
+                                                                      "confidence [0-1]\n\n"
+                                                                      "")
+def recognize_face(recognizeParam: yunet_sdk.RecognizeParam = Body(..., description="faces", example = {
+	"feature0": [],
+	"feature1": []
+}
+)):
+    return yunet_sdk.recognize_face(recognizeParam)
+
+
+# ----------------------------------------------------------------
+
+
+@app.post("/detect_face_insightface", summary='detect face', tags=['InsightFace'], response_description="""
+ 
+"""
+                                                                      "返回结果：\n\n"
+                                                                      "Faces: bouding box [0:4]\n\n"
+                                                                      "landmarks: 5 landmarks [4:14]\n\n"
+                                                                      "confidence [-1]\n\n"
+                                                                      "")
+def detect_face_insightface(detectParam: insightface_sdk.DetectionParam = Body(..., description="文件路径参数", example = {
+	"input": "/C/Intel/1.png",
+	"output": "/C/Intel/1-insightfaces.png",
+    "save": True
+}
+)):
+    # logging.info("sst_qa, {param}", param=qaParam.__dict__)
+    if(not validate_multi_file_path([detectParam.input, detectParam.output])):
+        return False
+    return insightface_sdk.detect_face(detectParam)
+
+@app.post("/compute_sim", summary='compute sim', tags=['InsightFace'], response_description=""
+                                                                      "返回结果：\n\n"
+                                                                      "confidence [0-1]\n\n"
+                                                                      "")
+def recognize_face(recognizeParam: insightface_sdk.RecognizeParam = Body(..., description="faces", example = {
+	"feature0": [],
+	"feature1": []
+}
+)):
+    return insightface_sdk.compute_sim(recognizeParam)
+
+
+# ----------------------------------------------------------------
+
+@app.post("/detect_image", summary='detect_image', tags=['NudeNet'], response_description=""
+                                                                      "返回结果：\n\n"
+                                                                      "图片信息\n\n"
+                                                                      "")
+def detect_image(nudeParam: nudenet_sdk.NudeParam = Body(..., description="参数", example = {
+	"input": "/app/data/img/1.png",
+}
+)):
+    # logging.info("sst_qa, {param}", param=qaParam.__dict__)
+    return nudenet_sdk.detect_image(nudeParam)
+
+@app.post("/detect_video", summary='detect_video', tags=['NudeNet'], response_description=""
+                                                                      "返回结果：\n\n"
+                                                                      "视频信息\n\n"
+                                                                      "")
+def detect_video(nudeParam: nudenet_sdk.NudeParam = Body(..., description="参数", example = {
+	"input": "/app/data/video/1.mp4",
+}
+)):
+    # logging.info("sst_qa, {param}", param=qaParam.__dict__)
+    return nudenet_sdk.detect_video(nudeParam)
+
+#----------------------------------------------------
+
+
+@app.post("/classify_image", summary='classify_image', tags=['NudeNet'], response_description=""
+                                                                      "返回结果：\n\n"
+                                                                      "图片信息\n\n"
+                                                                      "")
+def classify_image(nudeParam: nudenet_sdk.NudeParam = Body(..., description="参数", example = {
+	"input": "/app/data/img/1.png",
+}
+)):
+    # logging.info("sst_qa, {param}", param=qaParam.__dict__)
+    return nudenet_sdk.classify_image(nudeParam)
+
+@app.post("/classify_video", summary='classify_video', tags=['NudeNet'], response_description=""
+                                                                      "返回结果：\n\n"
+                                                                      "视频信息\n\n"
+                                                                      "")
+def classify_video(nudeParam: nudenet_sdk.NudeParam = Body(..., description="参数", example = {
+	"input": "/app/data/video/2.mp4",
+}
+)):
+    # logging.info("sst_qa, {param}", param=qaParam.__dict__)
+    return nudenet_sdk.classify_video(nudeParam)
+
+
 
 # validate file path, avoid system path or critical path
 def validate_file_path(file_path: str):
